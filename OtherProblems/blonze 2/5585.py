3d3705d4b1e344fbb5e508e416ce24cb5c28898f
HyunSeong <hhs950120@gmail.com>
2022-09-22 22:17:27 +0900

    수정

--- OtherProblems/blonze 2/5585.py
+++ OtherProblems/blonze 2/5585.py
@@ -2,30 +2,12 @@
 # 그리디
 
 change = 1000 - int(input())
-
+money = [500, 100, 50, 10, 5, 1]
 count = 0
 
-if change >= 500:
-    count += change // 500
-    change %= 500
-
-if change >= 100:
-    count += change // 100
-    change %= 100
-
-if change >= 50:
-    count += change // 50
-    change %= 50
-
-if change >= 10:
-    count += change // 10
-    change %= 10
-
-if change >= 5:
-    count += change // 5
-    change %= 5
-
-if change >= 1:
-    count += change
+for price in money:
+    if change >= price:
+        count += change // price
+        change %= price
 
 print(count)
